03b89c91db1142dc0655cb271d2edc551db456b3
Renio <eunho9684@gmail.com>
2025-07-16 01:26:56 +0900

    project, blog setting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,42 @@
+$(document).ready(function () {
+  blog_posts();
+  general_utils();
+});
 
-$(document).ready(function() {
-    general_utils();
-    blog_posts();
-})
+$(".skillbar").each(function () {
+  const percent = parseInt($(this).attr("data-percent")) / 100;
+  const fullWidth = $(this).width(); // skillbar 전체 width
+  const targetWidth = fullWidth * percent;
 
-
-function general_utils() {
-    // smooth scrolling for nav links
-    $('.head-menu-wrap a').smoothScroll();
-    $('.extra-link a').smoothScroll();
-    $('.profile-pic-link').smoothScroll();
-
-    $('.skillbar').each(function(){
-		$(this).find('.skillbar-bar').animate({
-			width: $(this).attr('data-percent')
-		}, 1000);
-	});
-}
+  $(this).find(".skillbar-bar").animate(
+    {
+      width: targetWidth,
+    },
+    1000
+  );
+});
 
 function blog_posts() {
+  // keeping it static, can be fetched from a blog dynamically as well
+  let posts = [
+    {
+      url: "_posts/25.07.16.html",
+      title: "First post",
+    },
+  ];
 
-    // keeping it static, can be fetched from a blog dynamically as well
-    let posts = [
-        {
-            url: 'https://www.nagekar.com/2017/02/trip-to-bramhatal-uttarakhand.html',
-            title: 'Trek To Bramhatal (Uttarakhand)',
-        },
-        {
-            url: 'https://www.nagekar.com/2017/08/privacy.html',
-            title: 'Privacy - How I Converted',
-        },
-        {
-            url: 'https://www.nagekar.com/2018/01/jagriti-yatra.html',
-            title: 'Jagriti Yatra 2017',
-        },
-        {
-            url: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-            title: 'Private Cloud Part 2 | Encrypted Storage With NextCloud',
-        },
-        {
-            url: 'https://www.nagekar.com/2018/07/eli5-how-https-works.html',
-            title: 'ELI5 - How HTTPS Works',
-        },
-    ];
+  let post_html = [];
 
-    let post_html = [];
+  for (let post of posts) {
+    let tags;
 
-    for(let post of posts) {
+    if (post.tags) {
+      tags = post.tags.map((tag) => {
+        return `<a href="https://www.nagekar.com/tags#${tag}">${tag}</a>`;
+      });
+    }
 
-        let tags;
-        
-        if(post.tags) {
-            tags = post.tags.map(tag => {
-                return `<a href="https://www.nagekar.com/tags#${tag}">${tag}</a>`
-            })
-        }
-
-        let post_template = `
+    let post_template = `
         <div class="blog-post" onclick="blog_link_click('${post.url}');">
 
             <div class="blog-link">
@@ -71,16 +51,16 @@ function blog_posts() {
         </div>
         `;
 
-        post_html.push(post_template);
-    }
+    post_html.push(post_template);
+  }
 
-    // for the more posts link
-    let post_template = `
+  // for the more posts link
+  let post_template = `
     <div class="blog-post more-blogs" onclick="blog_link_click('https://www.nagekar.com');">
 
         <div class="blog-link">
 
-            <h3><a href="https://www.nagekar.com">Visit the blog for more posts</a></h3>            
+            <h3><a href="">Visit the blog for more posts</a></h3>            
 
         </div>
 
@@ -90,12 +70,11 @@ function blog_posts() {
     </div>
     `;
 
-    post_html.push(post_template);
+  post_html.push(post_template);
 
-    $('#rss-feeds').html(post_html);
-
+  $("#rss-feeds").html(post_html.join(""));
 }
 
 function blog_link_click(url) {
-    window.location = url;
+  window.location = url;
 }
