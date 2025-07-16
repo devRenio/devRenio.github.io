276b76d66fe2bb1bd30eadf14e28fb24935c4fd7
Renio <eunho9684@gmail.com>
2025-07-16 14:05:32 +0900

    blog basic setting

---
+++ assets/js/featured-posts.js
@@ -0,0 +1,6 @@
+const featuredPosts = [
+  {
+    title: "First Post",
+    url: "/blog/posts/25.07.16.html",
+  },
+];
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,60 +4,34 @@ $(document).ready(function () {
 });
 
 function blog_posts() {
-  // keeping it static, can be fetched from a blog dynamically as well
-  let posts = [
-    {
-      url: "/posts/25.07.16.html",
-      title: "First post",
-    },
-  ];
-
   let post_html = [];
 
-  for (let post of posts) {
-    let tags;
-
-    if (post.tags) {
-      tags = post.tags.map((tag) => {
-        return `<a href="">${tag}</a>`;
-      });
-    }
-
+  for (let post of featuredPosts) {
     let post_template = `
-        <div class="blog-post" onclick="blog_link_click('${post.url}');">
-
-            <div class="blog-link">
-    
-                <h3><a href="${post.url}">${post.title}</a></h3>            
-
-            </div>
-    
-            <div class="blog-goto-link">
-                <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
-            </div>
+      <div class="blog-post" onclick="blog_link_click('${post.url}');">
+        <div class="blog-link">
+          <h3><a href="${post.url}">${post.title}</a></h3>            
         </div>
-        `;
-
+        <div class="blog-goto-link">
+          <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
+        </div>
+      </div>
+    `;
     post_html.push(post_template);
   }
 
-  // for the more posts link
-  let post_template = `
-    <div class="blog-post more-blogs" onclick="blog_link_click('');">
-
-        <div class="blog-link">
-
-            <h3><a href="">Visit the blog for more posts</a></h3>            
-
-        </div>
-
-        <div class="blog-goto-link">
-            <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
-        </div>
+  // '더 보기' 링크 추가
+  let more_template = `
+    <div class="blog-post more-blogs" onclick="blog_link_click('/blog');">
+      <div class="blog-link">
+        <h3><a href="/blog">Visit the blog for more posts</a></h3>            
+      </div>
+      <div class="blog-goto-link">
+        <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
+      </div>
     </div>
-    `;
-
-  post_html.push(post_template);
+  `;
+  post_html.push(more_template);
 
   $("#rss-feeds").html(post_html.join(""));
 
