2024e6e6433415c38bb0b85ba4c98caf32bb2456
devRenio <eunho9684@gmail.com>
2026-03-19 15:38:32 +0900

    home page renewal

--- Gemfile
+++ Gemfile
@@ -5,3 +5,8 @@ gem "jekyll-feed"
 gem 'tzinfo'
 gem 'tzinfo-data'
 gem "jekyll-paginate"
+gem "logger"
+
+group :jekyll_plugins do
+  gem "jekyll-paginate"
+end
--- assets/js/projects.js
+++ assets/js/projects.js
@@ -10,13 +10,22 @@ let render_projects = (slug) => {
 
   let projects_obj = [
     {
-      image: "assets/images/memorization.png",
+      image: "assets/images/memorization2.png",
+      link: "https://github.com/devRenio/devRenio.github.io/tree/main/samuel",
+      title: "Bible-verse-memorization(web)",
+      demo: "https://devrenio.github.io/samuel",
+      technologies: ["React"],
+      description: "React project for the Memorization at Samuel School.",
+      categories: ["featured", "react"],
+    },
+    {
+      image: "assets/images/memorization1.png",
       link: "https://github.com/devRenio/Bible-verse-memorization",
-      title: "Bible-verse-memorization",
-      demo: "",
+      title: "Bible-verse-memorization(app)",
+      demo: "https://github.com/devRenio/Bible-verse-memorization/releases/download/42SS/samuel_memorization.exe",
       technologies: ["Python"],
       description: "Python project for the Memorization at Samuel School.",
-      categories: ["featured", "python"],
+      categories: ["python"],
     },
   ];
 
@@ -50,54 +59,49 @@ let render_projects = (slug) => {
             start: "top 85%",
             once: true,
           },
-        }
+        },
       );
     });
 };
 
 let project_mapper = (project) => {
   return `
-        <div class="wrapper">
-                
-            <div class="card radius shadowDepth1">
+    <div class="wrapper">
+      <div class="card radius shadowDepth1">
+        ${
+          project.image
+            ? `<div class="card__image border-tlr-radius">
+                <a href="${project.link}">
+                    <img src="${project.image}" alt="image" id="project-image" class="border-tlr-radius">
+                </a>
+            </div>`
+            : ""
+        }
 
-                ${
-                  project.image
-                    ? `<div class="card__image border-tlr-radius">
-                        <a href="${project.link}">
-                            <img src="${project.image}" alt="image" id="project-image" class="border-tlr-radius">
-                        </a>
-                    </div>`
-                    : ""
-                }
+        <div class="card__content card__padding">
+          <article class="card__article">
+            <h2><a href="${project.link}">${project.title}</a></h2>
+            <p class="paragraph-text-normal">${project.description}</p>
+          </article>
 
-        
-                <div class="card__content card__padding">
-        
-                    <article class="card__article">
-                        <h2><a href="${project.link}">${project.title}</a></h2>
-        
-                        <p class="paragraph-text-normal">${
-                          project.description
-                        } ${
-    project.demo ? `<a href="${project.demo}">Demo</a>` : ""
-  }</p>
-                    </article>
-
-                                
-                    <div class="card__meta">
-                        ${project.technologies
-                          .map(
-                            (tech) =>
-                              `<span class="project-technology paragraph-text-normal">${tech}</span>`
-                          )
-                          .join("")}
-                    </div>
-
-                </div>
-            </div>
+          <div class="card__meta">
+            ${project.technologies
+              .map(
+                (tech) =>
+                  `<span class="project-technology paragraph-text-normal">${tech}</span>`,
+              )
+              .join("")}
+            
+            ${
+              project.demo
+                ? `<a href="${project.demo}" target="_blank" class="demo-button paragraph-text-normal">Demo</a>`
+                : ""
+            }
+          </div>
         </div>
-    `;
+      </div>
+    </div>
+  `;
 };
 
 let selected = (slug) => {
