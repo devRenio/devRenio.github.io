4484f855182b85d267f1945ccdc99b00fe454327
devRenio <eunho9684@gmail.com>
2026-08-10 15:19:07 +0900

    Add Goorm club site at /goorm

---
+++ goorm/_next/static/nqi51tbBk12vsy7APRBl_/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/goorm//_next/:path+",
+        "destination": "/goorm/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
